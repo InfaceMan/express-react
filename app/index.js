6f2b84177464712eeec53f6a3c265f5fee7bfcf2
InfaceMan <2796306900@qq.com>
2017-12-05 16:43:32 +0800

    Create index.js
    客户端获得服务端的html字符串后 再次render react组件

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import  React  from 'react';
+import App from './App';
+import { render }  from 'react-dom';
+
+
+const rootElement = document.getElementById('root');
+render(
+   <App/>,
+    rootElement
+);
